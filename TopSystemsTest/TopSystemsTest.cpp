e871fd22aa5644667e20c262bfa1c2bc3e92e89f
MaroonSkull <nikobelikkg@gmail.com>
2023-09-08 02:00:28 +0300

    Add project files.

---
+++ TopSystemsTest/TopSystemsTest.cpp
@@ -0,0 +1,12 @@
+﻿// TopSystemsTest.cpp : Defines the entry point for the application.
+//
+
+#include "TopSystemsTest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
