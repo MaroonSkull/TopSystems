ae8adbc80cf6045e82cd9b5cb18ac2b1620f34ce
MaroonSkull <nikobelikkg@gmail.com>
2023-09-18 21:53:44 +0300

    WIP: рендер канваса

--- TopSystemsTest/View/OpenGLView/ImguiView/openglImguiView.cpp
+++ TopSystemsTest/View/OpenGLView/ImguiView/openglImguiView.cpp
@@ -10,9 +10,12 @@ OpenglImguiView::OpenglImguiView() : GLFW_(glfw::init()) {
 		hints.contextVersionMajor = 3;
 		hints.contextVersionMinor = 3;
 		hints.openglProfile = glfw::OpenGlProfile::Core;
+		hints.openglForwardCompat = true;
 		hints.apply();
 
 		Window_ = new glfw::Window{ 640, 480, "Interview test" };
+		// первый проход фреймбуфера рендерим в размерах самого окна.
+		std::tie(frameWidth_, frameHeight_) = Window_->getFramebufferSize();
 		glfw::makeContextCurrent(*Window_);
 		glfw::swapInterval(1); // Enable vsync
 
@@ -32,7 +35,6 @@ OpenglImguiView::OpenglImguiView() : GLFW_(glfw::init()) {
 		Pipeline_->attachShader(Vertex_);
 
 		Pipeline_->linkProgram();
-		//Pipeline_->useProgram();
 
 		// Imgui
 		IMGUI_CHECKVERSION();
@@ -52,8 +54,12 @@ OpenglImguiView::OpenglImguiView() : GLFW_(glfw::init()) {
 		// Setup Platform/Renderer backends
 		if (!ImGui_ImplGlfw_InitForOpenGL(*Window_, true))
 			throw std::runtime_error{ "ImGui_ImplGlfw_InitForOpenGL return false." };
-		if (!ImGui_ImplOpenGL3_Init())
+		if (!ImGui_ImplOpenGL3_Init("#version 330"))
 			throw std::runtime_error{ "ImGui_ImplOpenGL3_Init return false." };
+
+		glViewport(0, 0, frameWidth_, frameHeight_);
+		create_triangle();
+		create_framebuffer();
 	}
 	catch (const glfw::Error& e) {
 		// todo replace "View" with file name macro
@@ -76,14 +82,90 @@ OpenglImguiView::~OpenglImguiView() {
 	// GlfwLibrary destructor calls glfwTerminate automatically
 }
 
-void OpenglImguiView::renderImgui(std::function<ImTextureID(ImVec2)> renderTexture) {
-	ImGui_ImplOpenGL3_NewFrame();
-	ImGui_ImplGlfw_NewFrame();
+void OpenglImguiView::create_triangle() {
+	GLfloat vertices[] = {
+		-1.0f, -1.0f, 0.0f, // 1. vertex x, y, z
+		1.0f, -1.0f, 0.0f, // 2. vertex ...
+		0.0f, 1.0f, 0.0f // etc... 
+	};
+
+	glGenVertexArrays(1, &VAO_);
+	glBindVertexArray(VAO_);
+
+	glGenBuffers(1, &VBO_);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+void OpenglImguiView::create_framebuffer() {
+	glGenFramebuffers(1, &FBO_);
+	glBindFramebuffer(GL_FRAMEBUFFER, FBO_);
+
+	// Текстура, в которую мы будем рендерить
+	glGenTextures(1, &textureId_);
+	// Рендербуфер для целей OpenGL
+	glGenRenderbuffers(1, &RBO_);
+	rescale_framebuffer();
+
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+		std::stringstream err;
+		err << "framebuffer is not complete, glCheckFramebufferStatus = " << glCheckFramebufferStatus(GL_FRAMEBUFFER);
+		throw std::runtime_error{ err.str() };
+	}
+
+	// unbind it all
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+}
+
+void OpenglImguiView::rescale_framebuffer() {
+	glBindTexture(GL_TEXTURE_2D, textureId_);
+	// Give an empty image to OpenGL ( the last "0" )
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frameWidth_, frameHeight_, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+	// Poor filtering. Needed !
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Set "renderedTexture" as our colour attachement #0
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureId_, 0);
+
+	glBindRenderbuffer(GL_RENDERBUFFER, RBO_);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, frameWidth_, frameHeight_);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO_);
+}
+
+void OpenglImguiView::renderImgui(ImTextureID renderTexture) {
 	ImGui::NewFrame();
 
-	UI_.DrawGUI(renderTexture);
+	auto sizes = UI_.DrawGUI(renderTexture);
+	if (frameWidth_ != sizes.x || frameHeight_ != sizes.y) {
+		isRescaled_ = true;
+		frameWidth_ = sizes.x;
+		frameHeight_ = sizes.y;
+	}
 
 	ImGui::Render();
+}
+
+void OpenglImguiView::draw() {
+	// отрисовываем GUI вместе с тем, что лежит в FBO с прошлого кадра
+	// решение некрасивое, но альтернатив для imgui я пока не нашёл
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	renderImgui((ImTextureID)NULL);
+		
+	// Render on the whole framebuffer
+	glViewport(0, 0, frameWidth_, frameHeight_);
+	// Render to our framebuffer
+	glBindFramebuffer(GL_FRAMEBUFFER, FBO_);
+
+
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 	if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
@@ -92,72 +174,6 @@ void OpenglImguiView::renderImgui(std::function<ImTextureID(ImVec2)> renderTextu
 		ImGui::RenderPlatformWindowsDefault();
 		glfw::makeContextCurrent(backupCurrentContext);
 	}
-}
-
-void OpenglImguiView::draw() {
-	// отрисовываем GUI вместе с FBO
-	// Здесь каждый раз создаётся новый буффер, но пока оставим как есть
-	// Стоит вынести его инициализацию в отдельный коллбэк, если размер frameSize будет меняться
-	renderImgui([&](ImVec2 frameSize) -> ImTextureID {
-		GLsizei frameWidth = frameSize.x;
-		GLsizei frameHeight = frameSize.y;
-
-		// The framebuffer, which regroups 0, 1, or more textures, and 0 or 1 depth buffer.
-		GLuint FramebufferName = 0;
-		glGenFramebuffers(1, &FramebufferName);
-		glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
-
-		// The texture we're going to render to
-		GLuint renderedTexture;
-		glGenTextures(1, &renderedTexture);
-
-		// "Bind" the newly created texture : all future texture functions will modify this texture
-		glBindTexture(GL_TEXTURE_2D, renderedTexture);
-
-		// Give an empty image to OpenGL ( the last "0" )
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, frameWidth, frameHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
-
-		// Poor filtering. Needed !
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-		// The depth buffer
-		GLuint depthrenderbuffer;
-		glGenRenderbuffers(1, &depthrenderbuffer);
-		glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
-		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, frameWidth, frameHeight);
-		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
-
-		// Set "renderedTexture" as our colour attachement #0
-		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, renderedTexture, 0);
-
-		// Set the list of draw buffers.
-		GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
-		glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
-
-		// Always check that our framebuffer is ok
-		if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-			std::stringstream err;
-			err << "framebuffer is corrupted, glCheckFramebufferStatus = " << glCheckFramebufferStatus(GL_FRAMEBUFFER);
-			throw std::runtime_error{ err.str() };
-		}
-		// Render to our framebuffer
-		glBindFramebuffer(GL_FRAMEBUFFER, FramebufferName);
-		glViewport(0, 0, frameWidth, frameHeight); // Render on the whole framebuffer, complete from the lower left corner to the upper right
-
-		//-----------------------------------
-		float vertices[] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
-		};
-
-
-
-
-		return (ImTextureID)renderedTexture;
-		return (ImTextureID)(GLuint)348585;
-	});
 
 	// меняем буфер для следующего раза
 	Window_->swapBuffers();
--- TopSystemsTest/View/OpenGLView/ImguiView/openglImguiView.hpp
+++ TopSystemsTest/View/OpenGLView/ImguiView/openglImguiView.hpp
@@ -15,12 +15,25 @@ class OpenglImguiView final : public IView {
 private:
 	glfw::GlfwLibrary GLFW_; // RAII
 	glfw::Window* Window_ = nullptr;
-	GUI UI_;
-	Shader *Vertex_ = nullptr;
-	Shader *Fragment_ = nullptr;
+	GUI UI_{};
+	Shader* Vertex_ = nullptr;
+	Shader* Fragment_ = nullptr;
 	GLProgram* Pipeline_ = nullptr;
+	GLsizei frameWidth_{};
+	GLsizei frameHeight_{};
+	GLuint VAO_{}; // vertex array object
+	GLuint VBO_{}; // vertex buffer object
+	GLuint FBO_{}; // frame buffer object
+	GLuint RBO_{}; // rendering buffer object
+	GLuint textureId_{}; // the texture id we'll need later to create a texture
+	bool isRescaled_{ false };
 
-	void renderImgui(std::function<ImTextureID(ImVec2)> renderTexture);
+	//creates the vertex arrays and buffers
+	void create_triangle();
+	void create_framebuffer();
+	void rescale_framebuffer();
+
+	void renderImgui(ImTextureID renderTexture);
 public:
 	// init glfwpp, glad, window, imgui
 	OpenglImguiView();
