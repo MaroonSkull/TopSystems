9c7a16f4b952c12adf44fb5a96d342fc7632fcb1
MaroonSkull <nikobelikkg@gmail.com>
2023-09-27 21:43:07 +0300

    structure: первый прототип того, что понадобится от модели

--- TopSystemsTest/Model/flatFigureModel.cpp
+++ TopSystemsTest/Model/flatFigureModel.cpp
@@ -1,2 +1,8 @@
 #include <flatFigureModel.hpp>
 
+const std::string FlatFigureModel::Triangle::name = { "Triangle" };
+const std::string FlatFigureModel::Quad::name = { "Quad" };
+const std::string FlatFigureModel::Circle::name = { "Circle" };
+const std::string FlatFigureModel::Ngon::name = { "Ngon" };
+const std::string FlatFigureModel::CurveBezier3::name = { "CurveBezier3" };
+const std::string FlatFigureModel::CurveBezier4::name = { "CurveBezier4" };
--- TopSystemsTest/Model/flatFigureModel.hpp
+++ TopSystemsTest/Model/flatFigureModel.hpp
@@ -1,8 +1,94 @@
 ﻿#pragma once
 #include <string>
+#include <variant>
+#include <vector>
 
-// Основная структура, от которой можно будет наследовать конкретные,
-// специфированные для различных окружностей, многоугольников, кривых и т.д.
-struct FlatFigure {
-	// общие данные тут
+
+class FlatFigureModel {
+public:
+	struct Point {
+		float x{ 0.f };
+		float y{ 0.f };
+		float z{ 0.f };
+	};
+
+	// структура, которая хранит сервисные данные о конкретной фигуре
+	template <class T>
+	class Figure : public T {
+	private:
+		inline static uint32_t counter{ 0 };
+	public:
+		const uint32_t id{ counter++ };
+		std::string name{ T.name + " №" + id};
+	};
+
+	struct Triangle {
+		const static std::string name;
+		Point first;
+		Point second;
+		Point third;
+	};
+
+	struct Quad {
+		const static std::string name;
+		Point first;
+		Point second;
+		Point third;
+		Point fourth;
+	};
+
+	struct Circle {
+		const static std::string name;
+		Point center;
+		float radius;
+	};
+
+	struct Ngon {
+		const static std::string name;
+		Point center;
+		Point first;
+		float n;
+		float radius;
+	};
+
+	struct CurveBezier3 {
+		const static std::string name;
+		Point start;
+		Point end;
+		Point first;
+	};
+
+	struct CurveBezier4 {
+		const static std::string name;
+		Point start;
+		Point end;
+		Point first;
+		Point second;
+	};
+
+	FlatFigureModel() = default;
+	~FlatFigureModel() = default;
+
+	template <class T>
+	Figure<T> *createFigure() {
+		Figures_.push_back(Figure<T>);
+		return Figures_.;
+	}
+private:
+	std::vector<
+		std::variant<
+			Triangle,
+			Quad,
+			Circle,
+			Ngon,
+			CurveBezier3,
+			CurveBezier4
+		>
+	> Figures_;
 };
+
+// вектор указателей на вариант
+// то же самое что лист по памяти
+
+// наследовать всё от фигуры, полиморфизм вместо варианта
+// вектор полиморфных указателей, тогда надо сильно меньше шаблонов, код проще
